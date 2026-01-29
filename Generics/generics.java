dac23acc45a8312256d42361efe0437b83e3ccf2
Marcus Prado <mgprado1@my.waketech.edu>
2026-01-29 16:13:47 +0000

    added more comments on generics

--- Generics/generics.java
+++ Generics/generics.java
@@ -9,14 +9,17 @@ import java.util.*;
 
 public class generics {
     public static void main(String[] args){
+        //Create Scanner and stack of user inputs
         Scanner input = new Scanner(System.in);
         GenericStack<String> stack = new GenericStack<>();
 
+        //Ask for input 5 times and push it into stack
         System.out.println("Enter five strings: ");
         for (int i = 1; i<=5; i++){
             stack.push(input.nextLine());
         }
 
+        //Output stack in reverse
         System.out.println("Outputting String in Reverse Order: ");
         while (!stack.isEmpty()){
             System.out.println(stack.pop());
