0b1a48d8928db7d006408f9de0f29226bb9c997a
Marcus Alexio Prado <mgprado1@my.waketech.edu>
2026-03-25 10:12:59 -0400

    mvc

--- MVC/Book.java
+++ MVC/Book.java
@@ -8,20 +8,39 @@ public class Book {
     //   - title  (String)
     //   - author (String)
     //   - year   (int)
-
+    private String title;
+    private String author;
+    private int year;
 
     // TODO (2): Write a constructor that accepts title, author, and year
     //           and assigns each to the corresponding field.
-
+    public Book(String title, String author, int year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 
     // TODO (3): Write public getters for all three fields.
     //   - getTitle()  → returns title
     //   - getAuthor() → returns author
     //   - getYear()   → returns year
+    public String getTitle(){
+        return this.title;
+    }
 
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
 
     // TODO (4): Write a public setter for year ONLY.
     //           (title and author should never change after construction)
     //   - setYear(int year)
+    public void setYear(int year){
+        this.year = year;
+    }
 
 }
